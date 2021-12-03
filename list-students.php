b94038f4795ac138862b9dee8c6a2e84ef458ef2
William <william.tomenh@gmail.com>
2021-12-02 21:09:42 -0300

    refactor: improve the students list.

--- list-students.php
+++ list-students.php
@@ -1,23 +1,9 @@
 <?php
 
-use Alura\Pdo\Domain\Model\Student;
-use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
+use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
 
 require_once 'vendor/autoload.php';
 
-$pdo = ConnectionCreator::createConnection();
+$studentRepository = new PdoStudentRepository();
 
-$stmt = $pdo->query('SELECT * FROM students;');
-
-$students = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$studentList = [];
-
-foreach ($students as $student) {
-    $studentList[] = new Student(
-        $student['id'],
-        $student['name'],
-        new \DateTimeImmutable($student['birth_date'])
-    );
-}
-
-var_dump($studentList);
+print_r($studentRepository->allStudents());
