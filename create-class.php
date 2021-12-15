4c0290d86f09cf697b07311f329cbf4c40c8776a
William <william.tomenh@gmail.com>
2021-12-14 23:44:31 -0300

    feat: create a student class using transactions sql.

---
+++ create-class.php
@@ -0,0 +1,20 @@
+<?php
+
+use Alura\Pdo\Domain\Model\Student;
+use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
+use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
+
+require_once 'vendor/autoload.php';
+
+$connection = ConnectionCreator::createConnection();
+
+$connection->beginTransaction();
+$repository = new PdoStudentRepository($connection);
+
+$student = new Student(null, 'Nico Steppat', new DateTimeImmutable('1985-10-01'));
+$repository->save($student);
+
+$anotherStudent = new Student(null, 'Sérgio Lopes', new DateTimeImmutable('1985-10-01'));
+$repository->save($anotherStudent);
+
+$connection->commit();
