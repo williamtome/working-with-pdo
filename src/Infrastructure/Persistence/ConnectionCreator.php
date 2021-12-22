c05ee1b15f1329b1208cfc1296d6a41acad7b00a
William <william.tomenh@gmail.com>
2021-12-22 07:48:16 -0300

    feat(ConnectionCreator): set attribute PDO on connection.

--- src/Infrastructure/Persistence/ConnectionCreator.php
+++ src/Infrastructure/Persistence/ConnectionCreator.php
@@ -9,6 +9,10 @@ class ConnectionCreator
     public static function createConnection(): PDO
     {
         $databasePath = __DIR__ . '/../../../db.sqlite';
-        return new PDO("sqlite:$databasePath");
+
+        $connection = new PDO("sqlite:$databasePath");
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $connection;
     }
 }
