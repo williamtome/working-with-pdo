255b039af43f56ce76b752eb7b870986270b687f
William <william.tomenh@gmail.com>
2021-12-28 18:32:05 -0300

    feat(students-with-phones): list only students with phones.

---
+++ students-with-phones.php
@@ -0,0 +1,11 @@
+<?php
+
+use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
+use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
+
+require_once 'vendor/autoload.php';
+
+$pdo = ConnectionCreator::createConnection();
+$repository = new PdoStudentRepository($pdo);
+
+print_r($repository->studentsWithPhones());
